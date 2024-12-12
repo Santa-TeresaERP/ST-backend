81288eb2664a225715d8f8e1c94e2096b6f4f929
jsusmachaca <falcorgd@gmail.com>
2024-12-12 12:16:42 -0500

    feat: add auth service

---
+++ src/services/useUser.ts
@@ -0,0 +1,37 @@
+import { generateToken } from '../config/authJwt'
+import User from '../models/user'
+import { AuthRequest, jwtData } from "../types/auth"
+import bcrypt from 'bcrypt'
+
+class useUser {
+  static async checkUser (body: AuthRequest) {
+    try {
+      const { email, password } = body.body
+      const user = await User.findOne({ where: { email }})
+  
+      if (!user) {
+        return null
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password)
+  
+      if (!isMatch) {
+        return null
+      }
+      
+      const { id, isAdmin, name } = user
+      const tokenData: jwtData = {
+        userId: id,
+        isAdmin: isAdmin,
+        name: name
+      }
+  
+      const token = generateToken(tokenData)
+      return { user: name, token }
+    } catch (error) {
+        throw error
+    }
+  }
+}
+
+export default useUser

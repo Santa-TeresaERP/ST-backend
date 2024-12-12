13f829e1d3075123035eac24c5a7e4a80a11988b
jsusmachaca <falcorgd@gmail.com>
2024-12-12 12:15:25 -0500

    refactor: improve route loader

--- src/config/routeLoader.ts
+++ src/config/routeLoader.ts
@@ -4,11 +4,21 @@ import { join } from 'path'
 
 const routesLoader = (app: Express) => {
   const dirsPath = join(process.cwd(), 'src', 'routes')
-  readdirSync(dirsPath).forEach(async (file) => {
-    const pathName = file.replace('.ts', '')
-    const path = await import(join(dirsPath, file))
-    app.use(`api/${pathName.toLowerCase()}`, path.default)
-  })
+
+  const loadRoutes = async () => {
+    const files = readdirSync(dirsPath)
+
+    for (const  file of files) {
+      if (file.endsWith('.ts')) {
+        const pathName = file.replace('.ts', '')
+        const route = await import(join(dirsPath, file))
+        app.use(`/${pathName}`, route.default)
+      }
+    }
+  }
+
+  loadRoutes()
+    .catch(err => console.error(`Error to load routes ${err}`))
 }
 
 export default routesLoader

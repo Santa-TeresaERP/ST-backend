274bd8cbcc11e15fe4b94c2fe23f7bed4a74c581
jsusmachaca <falcorgd@gmail.com>
2024-12-12 22:54:04 -0500

    fix: add next function in error handler

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 
-const errorHandler = (err: Error, _req: Request, res: Response) => {
+const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
   console.error(err.stack)
-  res.status(500).json({ error: 'Something went wrong!' })
+  res.status(500).json({ error: err.message })
 }
 
 export default errorHandler

bfac36487a46634d4c9dd9f16212de990cc28c9d
jsusmachaca <falcorgd@gmail.com>
2024-12-12 22:18:29 -0500

    feat: integrates prettier with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -12,4 +12,5 @@ export default [
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended
 ];

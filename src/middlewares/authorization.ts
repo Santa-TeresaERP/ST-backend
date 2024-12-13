3305aec9ce2e266fc40617201476f3a5e7adf800
jsusmachaca <falcorgd@gmail.com>
2024-12-12 22:26:07 -0500

    style: formating middleware

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -4,26 +4,27 @@ import { validateToken } from '@config/jwt'
 import { HttpError } from '@errors/http'
 
 const authorization = (req: AuthRequest, res: Response, next: NextFunction) => {
-    try {
-        const authHeader = req.header('Authorization')
-        if (!authHeader || !authHeader.startsWith('Bearer')) throw new HttpError('Token not provided', 401)
-        const token = authHeader.substring(7)
-        const validation = validateToken(token)
+  try {
+    const authHeader = req.header('Authorization')
+    if (!authHeader || !authHeader.startsWith('Bearer'))
+      throw new HttpError('Token not provided', 401)
+    const token = authHeader.substring(7)
+    const validation = validateToken(token)
 
-        if (!validation) throw new HttpError('The token is not valid', 401)
+    if (!validation) throw new HttpError('The token is not valid', 401)
 
-        req.authUser = validation
-        if (!req.authUser.isAdmin) {
-            throw new HttpError('The resource you are accessing does not exist', 404)
-        }
-        next()
-    } catch (err) {
-        if (err instanceof HttpError) {
-            res.status(err.statusCode).json({ error: err.message });
-        } else {
-            res.status(500).json({ error: 'Something was wrong' })
-        }
+    req.authUser = validation
+    if (!req.authUser.isAdmin) {
+      throw new HttpError('The resource you are accessing does not exist', 404)
     }
+    next()
+  } catch (err) {
+    if (err instanceof HttpError) {
+      res.status(err.statusCode).json({ error: err.message })
+    } else {
+      res.status(500).json({ error: 'Something was wrong' })
+    }
+  }
 }
 
 export default authorization
--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,9 +1,8 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
 const errorHandler = (err: Error, _req: Request, res: Response) => {
-    console.error(err.stack)
-    res.status(500).json({ message: 'Something went wrong!' })
-  }
-  
-export default errorHandler;
-  
+  console.error(err.stack)
+  res.status(500).json({ error: 'Something went wrong!' })
+}
+
+export default errorHandler

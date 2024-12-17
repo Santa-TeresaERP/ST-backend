420790179460ef982e61cd5808da81c27e13c226
Barreda Pinto Zoriel <zoriel.barreda@tecsup.edu.pe>
2024-12-17 16:08:39 -0500

    feat: schema user for validations

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,19 +1,34 @@
-/* 
-* EXAMPLE
-
+import { UserAttributes } from '@type/auth'
 import { z } from 'zod'
 
 const userSchema = z.object({
-  username: z.string({ required_error: 'username field is required' }),
-  first_name: z.string({ required_error: 'first_name field is required' }),
-  last_name: z.string({ required_error: 'last_name field is required' }),
-  email: z.string({ required_error: 'email field is required' }).email(),
-  password: z.string({ required_error: 'password field is required' }).min(6),
-  confirm_password: z.string({ required_error: 'confirm password is required' }).min(6)
-})
+  name: z
+    .string()
+    .max(45, 'El nombre no debe exceder los 45 caracteres')
+    .regex(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/, 'El nombre solo debe contener letras'),
 
-export const userValidation = (data: UserData) => {
-  return userSchema.refine(data => data.password === data.confirm_password, {
-    message: "passwords  don't match"
-  }).safeParse(data)
-} */
+  phonenumber: z
+    .string()
+    .length(9, 'El número telefónico debe tener 9 dígitos')
+    .regex(/^[0-9]+$/, 'El número telefónico solo debe contener números'),
+
+  dni: z
+    .string()
+    .length(8, 'El DNI debe tener 8 dígitos')
+    .regex(/^[0-9]+$/, 'El DNI solo debe contener números'),
+
+  email: z.string().email('El email debe tener un formato válido'),
+
+  password: z
+    .string()
+    .min(5, 'La contraseña debe tener al menos 5 caracteres')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d_-]+$/,
+      'La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y solo los caracteres "_" o "-" permitidos',
+    ),
+
+  isAdmin: z.boolean().optional(),
+  enabled: z.boolean().optional(),
+})
+export const userValidation = (data: UserAttributes) =>
+  userSchema.safeParse(data)

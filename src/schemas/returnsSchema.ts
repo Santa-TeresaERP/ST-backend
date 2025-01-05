4b132a67aa30157940eaaf12c3becea425b38e8c
OsCAR0367 <166433883+OsCAR0367@users.noreply.github.com>
2025-01-05 15:37:58 -0500

    Fix: add schema for returns-salesItems-sales

---
+++ src/schemas/returnsSchema.ts
@@ -0,0 +1,19 @@
+import { returnsAttributes } from '@type/returns'
+
+import { z } from 'zod'
+
+export const returnSchema = z.object({
+  id: z.string().uuid(),
+  productId: z.string().uuid(),
+  salesId: z.string().uuid(),
+  reason: z.string().nullable(),
+  observations: z.string().nullable(),
+  createdAt: z.date(),
+});
+
+export const productionValidation = (data: returnsAttributes) =>
+  returnSchema.safeParse(data)
+
+
+
+

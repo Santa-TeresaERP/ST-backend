1a88dc54aa515256f6c2623f8812d62847d2e65e
OsCAR0367 <166433883+OsCAR0367@users.noreply.github.com>
2025-01-05 15:46:12 -0500

    Fix: modify schema salesItems-sales

---
+++ src/schemas/salesSchema.ts
@@ -0,0 +1,13 @@
+import { salesAtributes } from "@type/sales";
+import { z } from "zod";
+
+export const salesSchema = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  total: z.number().positive(),
+  observations: z.string().nullable(),
+  createdAt: z.date(),
+});
+
+export const saleValidation = (data: salesAtributes) =>
+  salesSchema.safeParse(data);

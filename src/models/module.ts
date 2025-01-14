c36adab333d9deff28632b81a2ac51a72711a2a1
ceczzx <geraldo123villanueva@gmail.com>
2025-01-13 19:18:02 -0500

    fix:fix module conexion

--- src/models/module.ts
+++ src/models/module.ts
@@ -1,20 +1,22 @@
 import { DataTypes, Model, Optional } from 'sequelize'
 import sequelize from '@config/database'
-import { ModulesAttributes } from '@type/modules'
+import { ModuleAttributes } from '@type/modules'
 import { v4 as uuid } from 'uuid'
 
 class Module
-  extends Model<ModulesAttributes, Optional<ModulesAttributes, 'id'>>
-  implements ModulesAttributes
+  extends Model<ModuleAttributes, Optional<ModuleAttributes, 'id'>>
+  implements ModuleAttributes
 {
   public id!: string
   public name!: string
+  public descripcion?: string // Agrega la propiedad descripcion
 }
 
 Module.init(
   {
     id: { type: DataTypes.UUID, defaultValue: uuid, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false },
+    descripcion: { type: DataTypes.STRING, allowNull: true }, // Agrega la columna descripcion
   },
   {
     sequelize,

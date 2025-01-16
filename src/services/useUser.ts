21b851201f26b7b9fe75c910a61dbd11cbc0c4ed
yVentura7 <168231053+yVentura7@users.noreply.github.com>
2025-01-16 09:12:34 -0500

    feat/changepassword

--- src/services/useUser.ts
+++ src/services/useUser.ts
@@ -5,6 +5,7 @@ import User from '@models/user'
 import { jwtData, UserAttributes } from '@type/auth'
 import { userValidation, userValidationPartial } from 'src/schemas/userSchema'
 
+
 class useUser {
   static async checkUser(body: UserAttributes) {
     const validation = userValidationPartial(body)
@@ -101,6 +102,30 @@ class useUser {
     await user.update(body)
     return user
   }
+
+  static async changePassword(id: string, currentPassword: string, newPassword: string) {
+    // Buscar al usuario por ID
+    const user = await User.findByPk(id);
+    if (!user) {
+      return { error: 'Usuario no encontrado' }; // El usuario no existe
+    }
+  
+    // Verificar si la contraseña actual es correcta
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return { error: 'Contraseña actual incorrecta' }; // Contraseña actual no coincide
+    }
+  
+    // Generar el hash de la nueva contraseña
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+  
+    // Actualizar la contraseña del usuario
+    await user.update({ password: hashedPassword });
+  
+    return { message: 'Contraseña actualizada correctamente' };
+  }
 }
 
 export default useUser
+ 

15de35b05ec3740e20099bdad4879414647c86f8
MijaelJuy <mijael.juy@tecsup.edu.pe>
2025-04-10 23:46:20 -0500

    feat/tabla_sales

--- src/schemas/salesSchema.ts
+++ src/schemas/salesSchema.ts
@@ -1,22 +1,18 @@
-import { salesAtributes } from '@type/sale'
+import { salesAttributes } from '@type/sale'
 import { z } from 'zod'
 
 // Definir el esquema de ventas, si salesAtributes es un tipo, necesitas crear el esquema de validación correspondiente.
 const salesSchema = z.object({
-  id: z
-    .string()
-    .uuid('El ID debe ser un UUID válido'),
+  id: z.string().uuid('El ID debe ser un UUID válido'),
 
-  userId: z
-    .string()
-    .uuid('El ID de usuario debe ser un UUID válido'),
+  userId: z.string().uuid('El ID de usuario debe ser un UUID válido'),
 
   total: z
     .number()
     .positive('El total debe ser un número positivo')
     .refine(
       (val) => val.toString().length === 9,
-      'El número debe tener exactamente 9 dígitos'
+      'El número debe tener exactamente 9 dígitos',
     ),
 
   observations: z
@@ -24,16 +20,15 @@ const salesSchema = z.object({
     .nullable()
     .refine((val) => {
       if (val && /<script|<\/script|SELECT|DROP|INSERT|--/i.test(val)) {
-        return false;
+        return false
       }
-      return true;
+      return true
     }, 'Observaciones contienen caracteres no permitidos o posibles inyecciones'),
 
   createdAt: z
     .date()
-    .refine(date => date <= new Date(), 'La fecha no puede ser futura'),
-});
+    .refine((date) => date <= new Date(), 'La fecha no puede ser futura'),
+})
 
-
-export const saleValidation = (data: salesAtributes) =>
-  salesSchema.safeParse(data);
+export const saleValidation = (data: salesAttributes) =>
+  salesSchema.safeParse(data)

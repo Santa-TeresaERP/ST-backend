f14091ffce16f4aeb82579becfc57b150a671dcd
MijaelJuy <mijael.juy@tecsup.edu.pe>
2025-04-11 00:17:25 -0500

    feat/tabla_store

---
+++ src/schemas/storeSchema.ts
@@ -0,0 +1,42 @@
+import { store as StoreAttributes } from '@type/store'
+import { z } from 'zod'
+
+const storeSchema = z.object({
+  store_name: z
+    .string()
+    .max(100, 'El nombre de la tienda no debe exceder los 100 caracteres')
+    .min(1, 'El nombre de la tienda es obligatorio')
+    .regex(
+      /^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s\-.,]+$/,
+      'El nombre contiene caracteres no válidos',
+    )
+    .refine(
+      (val) => !/^\s|\s$/.test(val),
+      'El nombre no debe comenzar ni terminar con espacios en blanco',
+    ),
+
+  address: z
+    .string()
+    .max(200, 'La dirección no debe exceder los 200 caracteres')
+    .min(1, 'La dirección es obligatoria')
+    .refine(
+      (val) => !/^\s|\s$/.test(val),
+      'La dirección no debe comenzar ni terminar con espacios en blanco',
+    ),
+
+  observations: z
+    .string()
+    .max(300, 'Las observaciones no deben exceder los 300 caracteres')
+    .optional()
+    .refine(
+      (val) =>
+        val === undefined ||
+        !/<script.*?>.*?<\/script>|SELECT\s.*?FROM|DROP\sTABLE|INSERT\sINTO|--|;|\\|\/\*/i.test(
+          val,
+        ),
+      'Las observaciones contienen caracteres no permitidos o posibles inyecciones',
+    ),
+})
+
+export const storeValidation = (data: StoreAttributes) =>
+  storeSchema.safeParse(data)
---
+++ src/types/store.d.ts
@@ -0,0 +1,8 @@
+export interface store {
+  id?: string
+  store_name: string
+  address: string
+  observations?: string
+  createdAt?: Date
+  updatedAt?: Date
+}

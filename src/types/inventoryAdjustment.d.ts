ccfd3f9e02e4c78b43a9581fb2fa8491fc144519
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-04-23 16:21:18 -0500

    feat/invetory-and-payments-eliminated-service

--- src/types/inventoryAdjustment.d.ts
+++
@@ -1,9 +0,0 @@
-export interface InventoryAdjustmentAttributes {
-  id?: string
-  product_id: string
-  adjustment_type: string
-  quantity: number
-  observations?: string
-  createdAt?: Date
-  updatedAt?: Date
-}
--- src/types/payments.d.ts
+++
@@ -1,9 +0,0 @@
-export interface PaymentAttributes {
-  id?: string
-  rental_id: string
-  amount: number
-  payment_date: Date
-  status: string
-  createdAt?: Date
-  updatedAt?: Date
-}

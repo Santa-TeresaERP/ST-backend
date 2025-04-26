c473232d72c9dabd12cc0adfdb0cfb479150dc75
MijaelJuy <mijael.juy@tecsup.edu.pe>
2025-04-25 22:20:43 -0500

    feat/Crud Resource

---
+++ src/controllers/Resource/index.ts
@@ -0,0 +1,13 @@
+import CreateResource from './CreateResource'
+import GetResource from './GetResource'
+import UpdateResource from './UpdateResource'
+import DeleteResource from './DeleteResource'
+
+const index = {
+  CreateResource,
+  GetResource,
+  UpdateResource,
+  DeleteResource,
+}
+
+export default index

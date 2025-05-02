9838c1c3593b2720530c90ded0b14effcffca66a
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2025-05-02 15:42:06 -0500

    fix: routes better implementation

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,27 +5,11 @@ import productsController from '@controllers/Products/index'
 const router = express.Router()
 
 // Rutas de productos
-router.post('/api/products', authorization, productsController.createProduct)
-router.get('/api/products', authorization, productsController.getAllProduct)
-router.get(
-  '/api/products/confectionery',
-  authorization,
-  productsController.getConfectioneryProduct,
-)
-router.get(
-  '/api/products/:id',
-  authorization,
-  productsController.getProductByID,
-)
-router.patch(
-  '/api/products/:id',
-  authorization,
-  productsController.updateProduct,
-)
-router.delete(
-  '/api/products/:id',
-  authorization,
-  productsController.deleteProduct,
-)
+router.post('/', authorization, productsController.createProduct)
+router.get('/', authorization, productsController.getAllProduct)
+
+router.get('/:id', authorization, productsController.getProductByID)
+router.patch('/:id', authorization, productsController.updateProduct)
+router.delete('/:id', authorization, productsController.deleteProduct)
 
 export default router

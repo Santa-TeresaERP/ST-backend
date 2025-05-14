90ebd72ad45cf1c75aa3184700dbdf5924020b70
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-05-14 14:50:24 -0500

    fix/relations-added-in-movent

--- src/models/warehouseMovementProduct.ts
+++ src/models/warehouseMovementProduct.ts
@@ -2,6 +2,9 @@ import { DataTypes, Model, Optional } from 'sequelize'
 import sequelize from '@config/database'
 import { WarehouseMovomentProductAttributes } from '@type/almacen/warehouse_movement_product'
 import { v4 as uuid } from 'uuid'
+import Warehouse from '@models/warehouse'
+import Product from '@models/product'
+import Store from '@models/store'
 
 class WarehouseMovementProduct
   extends Model<
@@ -74,5 +77,35 @@ WarehouseMovementProduct.init(
     timestamps: true,
   },
 )
+
+WarehouseMovementProduct.belongsTo(Warehouse, {
+  foreignKey: 'warehouse_id',
+  as: 'warehouse',
+})
+
+Warehouse.hasMany(WarehouseMovementProduct, {
+  foreignKey: 'warehouse_id',
+  as: 'warehouse_movement_products',
+})
+
+WarehouseMovementProduct.belongsTo(Product, {
+  foreignKey: 'product_id',
+  as: 'product',
+})
+
+Product.hasMany(WarehouseMovementProduct, {
+  foreignKey: 'product_id',
+  as: 'warehouse_movement_products',
+})
+
+WarehouseMovementProduct.belongsTo(Store, {
+  foreignKey: 'store_id',
+  as: 'store',
+})
+
+Store.hasMany(WarehouseMovementProduct, {
+  foreignKey: 'store_id',
+  as: 'warehouse_movement_products',
+})
 
 export default WarehouseMovementProduct

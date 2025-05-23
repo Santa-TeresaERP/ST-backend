d3357ecd084006b468cf5c34f7a7bb2945e1fd1f
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-05-22 23:00:47 -0500

    refactor/getAllProduct-updated

--- src/services/Products/serviceGetAllProduct.ts
+++ src/services/Products/serviceGetAllProduct.ts
@@ -1,7 +1,32 @@
+import Category from '@models/categories'
 import Product from '@models/product'
+import RecipeProductConection from '@models/recipe_product_conections'
+import RecipeProductResource from '@models/recipe_product_resource'
+import Resource from '@models/resource'
 
 const serviceGetAllProduct = async () => {
-  const products = await Product.findAll()
+  const products = await Product.findAll({
+    attributes: ['id', 'name', 'description', 'price', 'imagen_url'],
+    include: [
+      { model: Category, as: 'category' },
+      {
+        model: RecipeProductResource,
+        as: 'recipe',
+        include: [
+          {
+            model: RecipeProductConection,
+            as: 'recipe_product_conections',
+            include: [
+              {
+                model: Resource,
+                as: 'resources',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  })
   return products
 }
 

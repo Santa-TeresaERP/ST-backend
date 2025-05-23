235e9704a1a3c0075ec02bcc1304467d41429f75
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-05-22 19:46:29 -0500

    fix/recipes-duplicated

--- src/services/Recipes/serviceGetRecipes.ts
+++
@@ -1,9 +0,0 @@
-import RecipeProductResource from '@models/recipe_product_resource'
-
-const serviceGetRecipes = async (): Promise<RecipeProductResource[]> => {
-  const recipes = await RecipeProductResource.findAll()
-
-  return recipes
-}
-
-export default serviceGetRecipes

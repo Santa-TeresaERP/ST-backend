b0f78f6d7b529f6becbcae14ebd842acbee481b0
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-06-02 22:28:11 -0500

    fix/lost-fix

--- src/controllers/Lost/getAllLost.ts
+++ src/controllers/Lost/getAllLost.ts
@@ -1,19 +1,9 @@
 import { Request, Response } from 'express'
 import useLost from '@services/Lost'
 
-const getAllLost = async (req: Request, res: Response) => {
+const getAllLost = async (_req: Request, res: Response) => {
   try {
-    const filters = {
-      product_id: req.query.product_id as string,
-      lost_type: req.query.lost_type as string,
-      start_date: req.query.start_date
-        ? new Date(req.query.start_date as string)
-        : undefined,
-      end_date: req.query.end_date
-        ? new Date(req.query.end_date as string)
-        : undefined,
-    }
-    const lostRecords = await useLost.getAllLost(filters)
+    const lostRecords = await useLost.getAllLost()
     res.status(200).json(lostRecords)
   } catch (error) {
     console.error('Error getting lost records:', error)
--- src/controllers/User/changePassword.ts
+++ src/controllers/User/changePassword.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import useUser from '@services/user/index'
+import useUser from '@services/User/index'
 
 const changePassword = async (req: Request, res: Response) => {
   try {

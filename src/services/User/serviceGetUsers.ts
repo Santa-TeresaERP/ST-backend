7d4fc206d322c562ecadfdc7d373fe9c585a994f
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-06-02 22:48:34 -0500

    fix/enviroment-&-getUsers

--- src/services/User/serviceGetUsers.ts
+++ src/services/User/serviceGetUsers.ts
@@ -1,7 +1,27 @@
+import Roles from '@models/roles'
 import User from '@models/user'
+import Permissions from '@models/permissions'
+import RolesPermissions from '@models/rolesPermissions'
 
-const serviceGetUsers = async () => {
-  const users = await User.findAll({ where: { status: true } })
+const serviceGetUsers = async (): Promise<User[]> => {
+  const users = await User.findAll({
+    where: { status: true },
+    include: [
+      {
+        model: Roles,
+        include: [
+          {
+            model: RolesPermissions,
+            include: [
+              {
+                model: Permissions,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  })
   return users
 }
 

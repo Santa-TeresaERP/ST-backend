408112dc692ecb898afddded1545cdb13ab57f60
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-06-20 16:32:36 -0500

    feat/buysResource_created

---
+++ src/types/almacen/buys_resource.d.ts
@@ -0,0 +1,11 @@
+export interface buysResourceAttributes {
+  id?: string
+  warehouse_id: string
+  resource_id: string
+  quantity: number
+  type_unit: string
+  unit_price: number
+  total_cost: number
+  supplier_id: string
+  entry_date: Date
+}

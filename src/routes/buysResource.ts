d353d1a64787ecb482275fe1c67da153ea8e0417
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-06-21 16:59:17 -0500

    refactor/buysResource-Controller-and-routes

---
+++ src/routes/buysResource.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import buysResourceController from '@controllers/buysResource'
+
+const router = Router()
+
+router.get('/', buysResourceController.GetBuysResources)
+router.post('/', buysResourceController.CreateBuysResource)
+router.put('/:id', buysResourceController.UpdateBuysResource)
+router.delete('/:id', buysResourceController.DeleteBuysResource)
+router.get('/:id', buysResourceController.GetBuysResourceById) // Nueva ruta
+
+export default router

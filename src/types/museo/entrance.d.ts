e96d262870229dbfa16b46cceb788524f29c0337
Cheems-code <osmar.ancco@tecsup.edu.pe>
2025-06-21 19:12:28 -0500

    Refactors entrance model and schema
    Updates the entrance model to align with the current data structure.
    
    Changes the 'sale_date' field to 'sale_data' and introduces 'reference_id'.
    
    Updates validation schema to match updated model fields, and enforces minimum length validation for sale_channel and payment_method.

--- src/types/museo/entrance.d.ts
+++ src/types/museo/entrance.d.ts
@@ -2,7 +2,8 @@ export interface entranceAttributes {
   id?: string
   user_id: string
   type_person_id: string
-  sale_date: string
+  reference_id: string
+  sale_data: string
   sale_number: string
   sale_channel: string
   total_sale: number

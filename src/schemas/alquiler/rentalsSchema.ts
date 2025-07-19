390edbac9475afe9ebe32243d2b58822f5ffdb79
nadiatorresalvarez <tu@email.com>
2025-07-19 18:46:59 -0500

    changes in the rental module

--- src/schemas/alquiler/rentalsSchema.ts
+++ src/schemas/alquiler/rentalsSchema.ts
@@ -10,6 +10,10 @@ const RentalSchema = z.object({
     message: 'El ID del lugar debe ser un UUID válido',
   }),
 
+  user_id: z.string().uuid({
+    message: 'El ID del usuario debe ser un UUID válido',
+  }),
+
   start_date: z.date({
     invalid_type_error: 'La fecha de inicio debe ser una fecha válida',
   }),
@@ -17,18 +21,6 @@ const RentalSchema = z.object({
   end_date: z.date({
     invalid_type_error: 'La fecha de fin debe ser una fecha válida',
   }),
-
-  income_id: z
-    .string()
-    .uuid({ message: 'El ID de ingreso debe ser un UUID válido' })
-    .optional(),
-
-  status: z.string().min(1, 'El estado es obligatorio'),
-
-  expense_id: z
-    .string()
-    .uuid({ message: 'El ID de gasto debe ser un UUID válido' })
-    .optional(),
 })
 
 export const rentalValidation = (data: RentalAttributes) =>

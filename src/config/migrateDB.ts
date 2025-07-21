b3531e9d95fe3865208e82cb6c4906f552f48938
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-07-21 11:16:12 -0500

    fix/error in merge

--- src/config/migrateDB.ts
+++ src/config/migrateDB.ts
@@ -4,6 +4,7 @@ import createAdmin from 'src/config/createAdmin'
 import { join } from 'node:path'
 import { readdirSync } from 'node:fs'
 import createModules from './createModules'
+import createRoles from './createRoles'
 import { createDefaultWarehouseAndPlant } from './warehouse'
 
 class migrate {
@@ -54,6 +55,7 @@ class migrate {
       `)
 
       await createAdmin()
+      await createRoles()
       await createModules()
       await createDefaultWarehouseAndPlant()
       console.log('Migration succesfully')

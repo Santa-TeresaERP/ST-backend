10b59e3e9aee71b2206b0801ff327d0a2db3c217
robbiejude murillo Gonza <168230819+ryukoark@users.noreply.github.com>
2025-07-25 11:29:38 -0500

    autorizacion de rol
    se asigno la autorizacion de rol a las  rutas de inventario

--- src/routes/warehouseProduct.ts
+++ src/routes/warehouseProduct.ts
@@ -1,45 +1,63 @@
 import express from 'express'
 import authorization from '@middlewares/authorization'
+import roleAuthorization from '@middlewares/roleAuthorization'
 import warehouseProductController from '@controllers/warehouse_Product'
 
 const router = express.Router()
 
-// CRUD básico
+// Crear registro
 router.post(
   '/',
   authorization,
+  roleAuthorization('canWrite', 'inventario'),
   warehouseProductController.createWarehouseProduct,
 )
+
+// Obtener todos
 router.get(
   '/',
   authorization,
+  roleAuthorization('canRead', 'inventario'),
   warehouseProductController.getAllWarehouseProducts,
 )
+
+// Obtener por ID
 router.get(
   '/:id',
   authorization,
+  roleAuthorization('canRead', 'inventario'),
   warehouseProductController.getWarehouseProductById,
 )
+
+// Actualizar
 router.patch(
   '/:id',
   authorization,
+  roleAuthorization('canEdit', 'inventario'),
   warehouseProductController.updateWarehouseProduct,
 )
+
+// Eliminar
 router.delete(
   '/:id',
   authorization,
+  roleAuthorization('canDelete', 'inventario'),
   warehouseProductController.deleteWarehouseProduct,
 )
 
-// Rutas específicas
+// Obtener productos por almacén
 router.get(
   '/warehouse/:warehouseId',
   authorization,
+  roleAuthorization('canRead', 'inventario'),
   warehouseProductController.getWarehouseProductsByWarehouse,
 )
+
+// Obtener productos por producto
 router.get(
   '/product/:productId',
   authorization,
+  roleAuthorization('canRead', 'inventario'),
   warehouseProductController.getWarehouseProductsByProduct,
 )
 
